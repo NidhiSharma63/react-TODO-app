ac1e2d956cee37f1e35264cce9377b57fc980496
NidhiSharma63 <nidhisharma639593@gmail.com>
2022-06-29 13:28:36 +0530

    userName added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import './App.css';
 import styled from "styled-components";
 import WelcomePage from './components/welcomePage';
+import MainPage from './components/MainPage';
+import useStore from './store';
 
 // change the min width to max width
 const MainContainer = styled.div`
-  
   min-width: 1440px;
   position: relative;
   height: 90vh;
-  // margin: auto;
 `
 
 function App() {
+  const name = useStore(state => state.userName);
+  console.log(name);
   return (
-    <div className="App">
-      <MainContainer className="main-container">
-        <WelcomePage />
-      </MainContainer>
-
-    </div>
+      <MainContainer>
+        {
+          name!==''?<MainPage />:<WelcomePage /> 
+        }
+        </MainContainer>
   );
 }
 
--- src/components/welcomePage.js
+++ src/components/welcomePage.js
@@ -1,9 +1,10 @@
 
 import React from 'react';
 import styled from "styled-components";
+import useStore from '../store';
 
 const Container = styled.div`
-  width: 100%;
+  width: 90%;
   position: absolute;
   height: 100%;
   z-index: 1;
@@ -24,17 +25,16 @@ const GlassEffect = styled.div`
   z-index: -1;
   background-color: #d7d7e611;
 `;
-
-
 const Input = styled.input`
   border:none;
   height: 4rem;
   outline: none;
-  font-size: 2rem;
-  padding-left: 1rem;
+  font-size: 2.5rem;
+  padding-left: .5rem;
   background: transparent;
   color:white;
   border-bottom:.5px solid white;
+  letter-spacing: .3rem;
 `
 const Col2 = styled.div`
   display: flex;
@@ -44,6 +44,9 @@ const Col2 = styled.div`
 `
 
 function WelcomePage() {
+  const setUserName = useStore(state => state.setUserName);
+  const [name, setName] = React.useState('');
+
   return (
    <>
      <Container>
@@ -51,8 +54,15 @@ function WelcomePage() {
         <Col2 >
           <p style={{fontSize:'5rem',marginBottom:'4rem'}}>please enter your name <span style={{fontSize:'3rem'}}>🖊️</span></p>
           <div>
-            <Input type="text" name="" id="" placeholder='your name...' />
-            <button className='btn' style={{marginLeft:'5rem'}}>submit</button>
+            <Input 
+            type="text" 
+            name="" id="" 
+            placeholder='your name...' 
+            onChange={(e)=>{setName(e.target.value)}}/>
+            <button 
+            className='btn' 
+            style={{marginLeft:'5rem'}}
+            onClick={()=>setUserName(name)}>submit</button>
           </div>
         </Col2>
       </Container>
